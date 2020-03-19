14e3db1b7cb9daecbcf3dc9ff3ac7d912d95247c
John Watterson <john@breakthrewbrew.com>
2020-03-18 19:36:02 -0600

    Initial commit, Card class completed

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+class Card {
+  constructor(id, question, answers, correctAnswer) {
+    this.id = id;
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer
+  }
+}
+
+module.exports = Card;
+//Each card has an id,
+// a question,
+// possible answers,
+// and a correct answer
